db8e810e3c456054c7f3e93c353a5a6472509957
s3822042 <s3822042@rmit.edu.vn>
2021-06-06 10:15:59 +0700

    finish up frontend

---
+++ frontend/src/app/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+  id: number;
+  name: string;
+  phone: string;
+  email: string;
+  address: string;
+  jobTitle: string;
+  imageUrl: string;
+  employeeCode: string;
+}
